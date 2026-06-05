09b3e96e5c7a4b5010d49f112d3fe49480459f37
shivangg7084 <shivangg0007@gmail.com>
2026-06-06 00:10:12 +0530

    LonPrefix

--- week-1/LonPrefix.java
+++ week-1/LonPrefix.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class LonPrefix {
-}
+        public String longestCommonPrefix(String[] strs) {
+            Arrays.sort(strs);
+            String str1 = strs[0];
+            String str2 = strs[strs.length-1];
+            int i = 0;
+            while(i<str1.length()){
+                if(str1.charAt(i) == str2.charAt(i)){
+                    i++;
+                }else{
+                    break;
+                }
+            }
+            return i==0?"":str1.substring(0,i);
+        }
+    }
+
